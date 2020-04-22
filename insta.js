8eb253a2e47d0b171df7dd5c376bf65e9ba999fa
leowyongfoo <61189340+leowyongfoo@users.noreply.github.com>
2020-04-22 17:59:54 +0800

    Delete insta.js

--- insta.js
+++
@@ -1,8 +0,0 @@
-let mainNavi = document.getElementById('js-main-nav');
-
-let navPh = document.getElementById('js-navbar-toggle');
-
-navPh = addEventListener('click', function()
-{
-	mainNavi.classList.toggle('active');
-});
